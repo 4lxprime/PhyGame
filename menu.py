635ec2f41838fe13c7405e95dc1e77e725cc8a21
4lxprime <idalxprime@gmail.com>
2022-10-21 18:32:59 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -29,15 +29,14 @@ top=canvas.create_rectangle(
 
 root.update()
 
-Tt=canvas.create_text(
-    root.winfo_width()/2, 40, 
+Tt=canvas.create_text(root.winfo_width()/2, 40, 
     text="PhyGame", 
-    font=('Helvetica 25 bold'), 
+    font=('Helvetica 30 bold'), 
     fill="white"
 )
 
-param=canvas.create_window(
-    root.winfo_width()/2+320, 40, 
+cred=canvas.create_window(
+    root.winfo_width()-60, root.winfo_height()-30, 
     window=Button(root, 
         font=("Arial", 15), 
         relief="flat", 
@@ -47,7 +46,7 @@ param=canvas.create_window(
         highlightthickness=1, 
         highlightbackground='black', 
         fg="white", 
-        text="Param"
+        text="Credits"
     )
 )
 
@@ -55,16 +54,31 @@ def STG():
     But1B['bg']="#1a1d21"
     But2B['bg']="#1e2227"
     But3B['bg']="#1e2227"
+    canvas.itemconfigure(Auto, state="normal")
+    canvas.itemconfigure(Manual, state="normal")
+    canvas.itemconfigure(HostPub, state="hidden")
+    canvas.itemconfigure(HostPrv, state="hidden")
+    canvas.itemconfigure(Ti, state="hidden")
 
 def HTG():
     But1B['bg']="#1e2227"
     But2B['bg']="#1a1d21"
     But3B['bg']="#1e2227"
+    canvas.itemconfigure(Auto, state="hidden")
+    canvas.itemconfigure(Manual, state="hidden")
+    canvas.itemconfigure(HostPub, state="normal")
+    canvas.itemconfigure(HostPrv, state="normal")
+    canvas.itemconfigure(Ti, state="hidden")
 
 def GI():
     But1B['bg']="#1e2227"
     But2B['bg']="#1e2227"
     But3B['bg']="#1a1d21"
+    canvas.itemconfigure(Auto, state="hidden")
+    canvas.itemconfigure(Manual, state="hidden")
+    canvas.itemconfigure(HostPub, state="hidden")
+    canvas.itemconfigure(HostPrv, state="hidden")
+    canvas.itemconfigure(Ti, state="normal")
 
 But1B=Button(root, 
     font=("Arial", 20), 
@@ -107,17 +121,20 @@ But3B=Button(root,
 
 But1=canvas.create_window(
     ((root.winfo_width()*(1/3))-root.winfo_width()/6), 107,
-    window=But1B
+    window=But1B,
+    state="normal"
 )
 
 But2=canvas.create_window(
     ((root.winfo_width()*(2/3))-root.winfo_width()/6), 107, 
-    window=But2B
+    window=But2B,
+    state="normal"
 )
 
 But3=canvas.create_window(
     ((root.winfo_width()*(3/3))-root.winfo_width()/6), 107, 
-    window=But3B
+    window=But3B,
+    state="normal"
 )
 
 Auto=canvas.create_window(
@@ -131,7 +148,8 @@ Auto=canvas.create_window(
         highlightthickness=1, 
         highlightbackground='black', 
         fg="white", 
-        text="Publics Servers"
+        text="Publics Servers",
+        state="normal"
     )
 )
 
@@ -146,10 +164,53 @@ Manual=canvas.create_window(
         highlightthickness=1, 
         highlightbackground='black', 
         fg="white", 
-        text="Manual Servers"
+        text="Manual Servers",
+        state="normal"
     )
 )
 
+HostPub=canvas.create_window(
+    root.winfo_width()/2-160, root.winfo_height()/2, 
+    window=Button(root, 
+        font=("Arial", 20), 
+        relief="flat", 
+        borderwidth=0, 
+        bg="#1e2227", 
+        width=18, 
+        highlightthickness=1, 
+        highlightbackground='black', 
+        fg="white", 
+        text="Host Public Game",
+        state="normal"
+    )
+)
+
+HostPrv=canvas.create_window(
+    root.winfo_width()/2+160, root.winfo_height()/2, 
+    window=Button(root, 
+        font=("Arial", 20), 
+        relief="flat", 
+        borderwidth=0, 
+        bg="#1e2227", 
+        width=18, 
+        highlightthickness=1, 
+        highlightbackground='black', 
+        fg="white", 
+        text="Host Private Game",
+        state="normal"
+    )
+)
+
+Ti=canvas.create_text(root.winfo_width()/2, root.winfo_height()/2, 
+    text="Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla\nBla Bla Bla Bla Bla Bla Bla Bla Bla Bla\nBla Bla Bla Bla Bla Bla Bla Bla Bla Bla\nBla Bla Bla Bla Bla Bla Bla Bla Bla Bla", 
+    font=("Arial", 25), 
+    fill="white",
+    state="normal"
+)
+
+canvas.itemconfigure(HostPub, state="hidden")
+canvas.itemconfigure(HostPrv, state="hidden")
+canvas.itemconfigure(Ti, state="hidden")
 
 
 GPage=canvas.create_window(2, 80, anchor="nw", width=root.winfo_width()-5, height=root.winfo_height()-82)
@@ -160,7 +221,9 @@ def act():
         canvas.itemconfigure(GPage, width=root.winfo_width()-5, height=root.winfo_height()-122)
         canvas.coords(Auto, root.winfo_width()/2-150, root.winfo_height()/2)
         canvas.coords(Manual, root.winfo_width()/2+150, root.winfo_height()/2)
-        canvas.coords(param, root.winfo_width()/2+320, canvas.coords(param)[1])
+        canvas.coords(HostPub, root.winfo_width()/2-150, root.winfo_height()/2)
+        canvas.coords(HostPrv, root.winfo_width()/2+150, root.winfo_height()/2)
+        canvas.coords(cred, root.winfo_width()-60, root.winfo_height()-30)
         canvas.coords(Tt, root.winfo_width()/2, canvas.coords(Tt)[1])
         canvas.coords(But1, (root.winfo_width()*(1/3))-root.winfo_width()/6, canvas.coords(But1)[1])
         canvas.coords(But2, (root.winfo_width()*(2/3))-root.winfo_width()/6, canvas.coords(But2)[1])
@@ -168,6 +231,7 @@ def act():
         But1B['width']=round(root.winfo_width()/49)
         But2B['width']=round(root.winfo_width()/49)
         But3B['width']=round(root.winfo_width()/49)
+        canvas.coords(Ti, root.winfo_width()/2, root.winfo_height()/2)
     
 t=threading.Thread(target=act)
 t.setDaemon(True)
