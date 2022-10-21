b465d38f418774d2e305220784b9a9b47d6915b1
4lxprime <idalxprime@gmail.com>
2022-10-22 00:42:17 +0200

    Update game.py

--- player/game.py
+++ player/game.py
@@ -303,6 +303,44 @@ class Map:
     def __init__(self):
         for y in range(1, 4, 2):
             Wall(ursina.Vec3(6, y, 0))
+            Wall(ursina.Vec3(6, y, 2))
+            Wall(ursina.Vec3(6, y, 4))
+            Wall(ursina.Vec3(6, y, 6))
+            Wall(ursina.Vec3(6, y, 8))
+
+            Wall(ursina.Vec3(4, y, 8))
+            Wall(ursina.Vec3(2, y, 8))
+            Wall(ursina.Vec3(0, y, 8))
+            Wall(ursina.Vec3(-2, y, 8))
+
+            Wall(ursina.Vec3(4, y, 14))
+            Wall(ursina.Vec3(2, y, 14))
+            Wall(ursina.Vec3(0, y, 14))
+            Wall(ursina.Vec3(-2, y, 14))
+
+            Wall(ursina.Vec3(0, y, -8))
+            Wall(ursina.Vec3(-2, y, -8))
+            Wall(ursina.Vec3(-4, y, -8))
+            Wall(ursina.Vec3(-6, y, -8))
+            Wall(ursina.Vec3(-8, y, -8))
+
+            Wall(ursina.Vec3(14, y, 2))
+            Wall(ursina.Vec3(14, y, 4))
+            Wall(ursina.Vec3(14, y, 6))
+            Wall(ursina.Vec3(14, y, 8))
+            Wall(ursina.Vec3(14, y, 10))
+
+            Wall(ursina.Vec3(-14, y, 2))
+            Wall(ursina.Vec3(-14, y, 4))
+            Wall(ursina.Vec3(-14, y, 6))
+            Wall(ursina.Vec3(-14, y, 8))
+            Wall(ursina.Vec3(-14, y, 10))
+
+            Wall(ursina.Vec3(-14, y, -4))
+            Wall(ursina.Vec3(-14, y, -6))
+            Wall(ursina.Vec3(-14, y, -8))
+            Wall(ursina.Vec3(-14, y, -10))
+            Wall(ursina.Vec3(-14, y, -12))
 
 
 class FloorCube(ursina.Entity):
